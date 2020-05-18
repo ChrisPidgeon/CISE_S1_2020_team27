8c998d280795b5eceb9e9a71c3a03bfab2a199ed
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-05-18 19:41:44 +1200

    Enabling Heroku deployment. Django-heroku added

--- seer_site/seer_site/settings.py
+++ seer_site/seer_site/settings.py
@@ -114,8 +114,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+import django_heroku
+django_heroku.settings(locals())
