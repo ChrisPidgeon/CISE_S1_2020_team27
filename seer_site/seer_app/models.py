780e91174035ac6166c7322c09183541229bf186
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-05-18 15:55:41 +1200

    Changes to models.py structure, Django migrations

--- seer_site/seer_app/models.py
+++ seer_site/seer_app/models.py
@@ -20,43 +20,42 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-
 class User(models.Model):
-    User_ID = models.IntegerField(max_length = 4, primary_key =True)
+    User_ID = models.IntegerField(primary_key =True)
     F_name = models.CharField(max_length = 40)
     L_name = models.CharField(max_length = 40)
     Birth_date = models.DateTimeField
     Email = models.CharField(max_length = 80)
 
 class Account(models.Model):
-    Account_ID = models.IntegerField(max_length = 4, primary_key = True)
-    User_Id = models.ForeignKey()
+    Account_ID = models.IntegerField(primary_key = True)
+    User_Id = models.ForeignKey(User, on_delete=models.CASCADE)
     Username = models.CharField(max_length = 40)
     Password = models.CharField(max_length = 40)
 
-class AccountRoles(models.Model):
-    Account_ID = models.ForeignKey()
-    Role_ID = models.ForeignKey()
-
 class Roles(models.Model):
-    Role_ID = models.IntegerField(max_length = 4, primary_key = True)
+    Role_ID = models.IntegerField(primary_key = True)
     Role_Description = models.CharField(max_length = 40)
     ####integrate an enum here
 
-class Search(models.Model):
-    Search_ID = models.IntegerField(max_length = 4, primary_key = True)
-    Keywords = models.CharField(max_length = 50)
-    User_ID = models.ForeignKey()
-    Article_ID = models.ForeignKey()
+class AccountRoles(models.Model):
+    Account_ID = models.ForeignKey(Account, on_delete=models.CASCADE)
+    Role_ID = models.ForeignKey(Roles, on_delete=models.CASCADE)
 
 class Article(models.Model):
-    Article_ID = models.IntegerField(max_length = 4,primary_key = True)
+    Article_ID = models.IntegerField(primary_key = True)
     Title = models.CharField(max_length = 100)
     Author = models.CharField(max_length = 100)
     Publication_date = models.DateTimeField
     Journal = models.CharField(max_length = 200)
-    Volume = models.IntegerField(max_length = 3)
-    Issue = models.IntegerField(max_length = 3)
+    Volume = models.IntegerField()
+    Issue = models.IntegerField()
     Article_Description = models.TextField(max_length = 800)
-    User_ID = models.ForeignKey()
+    User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
     ###Status Integrate an enum here
+
+class Search(models.Model):
+    Search_ID = models.IntegerField(primary_key = True)
+    Keywords = models.CharField(max_length = 50)
+    User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
+    Article_ID = models.ForeignKey(Article, on_delete=models.CASCADE)
