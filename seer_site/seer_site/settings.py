c741634a05eab6843a276c04b1c1ffbbb201ddf4
wwg3242 <61812377+wwg3242@users.noreply.github.com>
2020-05-18 12:25:48 +1200

    commit

--- seer_site/seer_site/settings.py
+++ seer_site/seer_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'seer_app.apps.SeerAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
