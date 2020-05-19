a6eab5da4500a915a935d3aaca4892122263846c
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-05-19 15:25:33 +1200

    Updating Models.py structure and table features

--- seer_site/seer_app/models.py
+++ seer_site/seer_app/models.py
@@ -2,29 +2,31 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from enum import Enum
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class User(models.Model):
-    User_ID = models.IntegerField(primary_key = True)
-    F_name = models.CharField(max_length = 40)
-    L_name = models.CharField(max_length = 40)
+    User_ID = models.AutoField(primary_key = True)
+    F_name = models.CharField(max_length = 40, default = '', null = False)
+    L_name = models.CharField(max_length = 40, default = '', null = False)
     Birth_date = models.DateTimeField
-    Email = models.CharField(max_length = 80)
+    Email = models.CharField(max_length = 80, default = '', null = False)
     def __str__(self):
         return "Name: {} {}/nBirth Date: {}/nEmail: {}/n".format(self.F_name, self.L_name, self.Birth_date, self.Email)
-    
+
 class Account(models.Model):
-    Account_ID = models.IntegerField(primary_key = True)
+    Account_ID = models.AutoField(primary_key = True)
     User_Id = models.ForeignKey(User, on_delete=models.CASCADE)
-    Username = models.CharField(max_length = 40)
-    Password = models.CharField(max_length = 40)
+    Username = models.CharField(max_length = 40, default = '', null = False)
+    Password = models.CharField(max_length = 40, default = '', null = False)
     def _str_(self):
         return "Account ID: {}/nUsername: {}/n".format(self.Account_ID, self.Username)
 
 class Roles(models.Model):
-    Role_ID = models.IntegerField(primary_key = True)
-    Role_Description = models.CharField(max_length = 40)
+    Role_ID = models.AutoField(primary_key = True)
+    Role_Description = models.CharField(max_length = 40, default = '', null = False)
     def _str_(self):
         return "Role: {}/n".format(self.Role_Description)
 
@@ -35,22 +37,28 @@ class AccountRoles(models.Model):
         return "Account ID: {}/nRole ID: {}/n".format(self.Account_ID, self.Role_ID)
 
 class Article(models.Model):
-    Article_ID = models.IntegerField(primary_key = True)
-    Title = models.CharField(max_length = 100)
-    Author = models.CharField(max_length = 100)
+    class ArticleStatus(models.TextChoices):
+        SUBMITTED = 'SUB', _('SUBMITTED')
+        IN_MODERATION = 'IN_MOD', _('IN MODERATION')
+        MODERATED = 'MOD', _('MODERATED')
+        ANALYZED = 'ANA', _('ANALYZED')
+        SAVED = 'SAV', _('SAVED')
+    Article_ID = models.AutoField(primary_key = True)
+    Title = models.CharField(max_length = 100, default = '', null = False)
+    Author = models.CharField(max_length = 100, default = '', null = False)
     Publication_date = models.DateTimeField
-    Journal = models.CharField(max_length = 200)
-    Volume = models.IntegerField()
-    Issue = models.IntegerField()
-    Article_Description = models.TextField(max_length = 800)
+    Journal = models.CharField(max_length = 200, default = '', null = False)
+    Volume = models.IntegerField(null = False)
+    Issue = models.IntegerField(null = False)
+    Article_Description = models.TextField(max_length = 800, default = '', null = False)
     User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
-    ###Status Integrate an enum here
+    Status = models.CharField(choices = ArticleStatus.choices, default = ArticleStatus.SAVED)
     def _str_(self):
         return self.Article_Description
 
 class Search(models.Model):
-    Search_ID = models.IntegerField(primary_key = True)
-    Keywords = models.CharField(max_length = 100)
+    Search_ID = models.AutoField(primary_key = True)
+    Keywords = models.CharField(max_length = 100, default = '')
     User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
     Article_ID = models.ForeignKey(Article, on_delete=models.CASCADE)
     def _str_(self):
