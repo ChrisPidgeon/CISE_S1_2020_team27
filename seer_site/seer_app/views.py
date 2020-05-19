f33debce3c2d5ac707812d01cb87c7fdcb8b92c2
Vaandite <53281594+Vaandite@users.noreply.github.com>
2020-05-20 10:53:56 +1200

    Creating Home.html and connecting

--- seer_site/seer_app/views.py
+++ seer_site/seer_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to SEER.")
+    return render(request, 'seer_app/Home.html')
