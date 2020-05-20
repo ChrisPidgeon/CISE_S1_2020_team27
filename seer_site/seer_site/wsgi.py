a7dc33f7ce0ee2b1cf7943a8b454994f12ee3af6
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-05-20 15:47:15 +1200

    Procfile config again

--- seer_site/seer_site/wsgi.py
+++ seer_site/seer_site/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seer_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seer_site.seer_site.settings')
 
 application = get_wsgi_application()
