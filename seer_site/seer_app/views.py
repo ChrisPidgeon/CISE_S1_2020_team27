f5027a05060dfb5e95d0c9e3ecc067b2465fcb01
Vaandite <53281594+Vaandite@users.noreply.github.com>
2020-05-20 23:12:21 +1200

    read article data from model and display in Home

--- seer_site/seer_app/views.py
+++ seer_site/seer_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from seer_app.models import Article
 
 def index(request):
-    return render(request, 'seer_app/Home.html')
+    #Luis 20/05/2020 added Articles model table to view
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render(request, 'seer_app/Home.html', context)
