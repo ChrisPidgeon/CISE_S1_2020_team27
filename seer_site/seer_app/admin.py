d35d51ea38c63b7724d2b22dafc4145cfb22bdd4
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-06-01 17:02:19 +1200

    Significant changes to admin display and model format

--- seer_site/seer_app/admin.py
+++ seer_site/seer_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib import messages
+from django.utils.translation import ngettext
 
 from .models import User
 from .models import Account
@@ -7,9 +9,66 @@ from .models import AccountRoles
 from .models import Article
 from .models import Search
 
-admin.site.register(User)
-admin.site.register(Account)
-admin.site.register(Roles)
-admin.site.register(AccountRoles)
-admin.site.register(Article)
-admin.site.register(Search)
+admin.site.site_header = "SEER Administration"
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('User_ID', 'F_name', 'L_name', 'Email')
+    list_filter = ('Created',)
+
+# class CustomModelChoiceField(forms.ModelChoiceField):
+#      def label_from_instance(self, obj):
+#             return "%s %s" % (obj.f_name, obj.l_name)
+
+# class AccountAdminForm(forms.ModelForm):
+#     account = CustomModelChoiceField(queryset=Account.objects.all())
+#     class Meta():
+#         model = Account
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('Account_ID', 'User_ID', 'Username')
+    list_filter = ('Created',)
+#    form = AccountAdminForm
+
+class RolesAdmin(admin.ModelAdmin):
+    list_display = ('Role_ID', 'Role_Type')
+    list_filter = ('Created',)
+
+class AccountRolesAdmin(admin.ModelAdmin):
+    list_display = ('Account_ID', 'Role_ID')
+    list_filter = ('Created',)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('Article_ID', 'Title', 'Status')
+    list_filter = ('Created',)
+   
+    def make_submitted(self, request, queryset):
+        queryset.update(Status = "SUB")
+
+    def make_in_moderation(self, request, queryset):
+        queryset.update(Status = "IN_MOD")
+    make_in_moderation.short_description = "Mark selected articles as in moderation"
+
+    def make_moderated(self, request, queryset):
+        queryset.update(Status = "MOD")
+    make_moderated.short_description = "Mark selected articles as moderated"
+
+    def make_analyzed(self, request, queryset):
+        queryset.update(Status = "ANA")
+    make_analyzed.short_description = "Mark selected articles as analyzed"
+
+    def make_saved(self, request, queryset):
+        queryset.update(Status = "SAV")
+    make_saved.short_description = "Mark selected articles as saved"
+
+    actions = [make_submitted, make_in_moderation, make_moderated, make_analyzed, make_saved]
+
+class SearchAdmin(admin.ModelAdmin):
+    list_display = ('Search_ID', 'Keywords')
+    list_filter = ('Created',)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Roles, RolesAdmin)
+admin.site.register(AccountRoles, AccountRolesAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Search, SearchAdmin)
--- seer_site/seer_app/models.py
+++ seer_site/seer_app/models.py
@@ -10,64 +10,98 @@ import django
 # Create your models here.
 
 class User(models.Model):
-    User_ID = models.AutoField(primary_key = True)
-    F_name = models.CharField(max_length = 40, default = '', null = False)
-    L_name = models.CharField(max_length = 40, default = '', null = False)
+    class Meta():
+        verbose_name_plural = "Users"
+    
+    User_ID = models.AutoField(primary_key = True, verbose_name=("User ID"))
+    F_name = models.CharField(max_length = 40, default = '', null = False, verbose_name=("First Name"))
+    L_name = models.CharField(max_length = 40, default = '', null = False, verbose_name=("Last Name"))
     Birth_date = models.DateField('Birth Date', default = django.utils.timezone.now)
     Email = models.CharField(max_length = 80, default = '', null = False)
+    Created = models.DateTimeField(auto_now_add = True, verbose_name = "Creation Date")
     def __str__(self):
         return self.F_name + ' ' + self.L_name 
     objects = models.Manager()
 
+
 class Account(models.Model):
-    Account_ID = models.AutoField(primary_key = True)
-    User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
-    Username = models.CharField(max_length = 40, default = '', null = False)
+    class Meta():
+        verbose_name_plural = "Accounts"
+
+    Account_ID = models.AutoField(primary_key = True, verbose_name=("Account ID"))
+    User_ID = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=("User ID"))
+    Username = models.CharField(max_length = 40, default = '', null = False, verbose_name=("User Name"))
     Password = models.CharField(max_length = 40, default = '', null = False)
+    Created = models.DateTimeField(auto_now_add = True, verbose_name = "Creation Date")
     def _str_(self):
         return self.Username
     objects = models.Manager()
 
 class Roles(models.Model):
-    Role_ID = models.AutoField(primary_key = True)
-    Role_Description = models.CharField(max_length = 40, default = '', null = False)
+    class Meta():
+        verbose_name = "Role"
+        verbose_name_plural = "Roles"
+
+    class RoleType(models.TextChoices):
+        SUBMITTER = 'ROLE_S', _('Submitter')
+        MODERATER = 'ROLE_M', _('Moderator')
+        ANALYST = 'ROLE_A', _('Analyst')
+        GENERAL_USER = 'ROLE_G', _('General User')
+
+    Role_ID = models.AutoField(primary_key = True, verbose_name=("Role ID"))
+    Role_Type = models.CharField(max_length = 6, choices = RoleType.choices, default = RoleType.GENERAL_USER)
+    Created = models.DateTimeField(auto_now_add = True, verbose_name = "Creation Date")
     def _str_(self):
-        return self.Role_Description
+        return self.Role_Type
     objects = models.Manager()    
 
 class AccountRoles(models.Model):
-    Account_ID = models.ForeignKey(Account, on_delete=models.CASCADE)
-    Role_ID = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    class Meta():
+        verbose_name = "Account Role"
+        verbose_name_plural = "Account Roles"
+
+    Account_ID = models.ForeignKey(Account, on_delete=models.CASCADE, verbose_name=("Account ID"))
+    Role_ID = models.ForeignKey(Roles, on_delete=models.CASCADE, verbose_name=("Role ID"))
+    Created = models.DateTimeField(auto_now_add = True, verbose_name = "Creation Date")
     def _str_(self):
-        return self.Account_ID + ' ' + self.Role_ID
+        return "Account ID: {}".format(self.Account_ID)
     objects = models.Manager()
 
 class Article(models.Model):
+    class Meta():
+        verbose_name_plural = "Articles"
+
     class ArticleStatus(models.TextChoices):
         SUBMITTED = 'SUB', _('SUBMITTED')
         IN_MODERATION = 'IN_MOD', _('IN MODERATION')
         MODERATED = 'MOD', _('MODERATED')
         ANALYZED = 'ANA', _('ANALYZED')
         SAVED = 'SAV', _('SAVED')
-    Article_ID = models.AutoField(primary_key = True)
+
+    Article_ID = models.AutoField(primary_key = True, verbose_name=("Article ID"))
     Title = models.CharField(max_length = 100, default = '', null = False)
     Author = models.CharField(max_length = 100, default = '', null = False)
     Publication_date = models.DateField('Publication Date', default = django.utils.timezone.now)
     Journal = models.CharField(max_length = 200, default = '', null = False)
     Volume = models.IntegerField(null = False)
     Issue = models.IntegerField(null = False)
-    Article_Description = models.TextField(max_length = 800, default = '', null = False)
-    User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
+    Article_Description = models.TextField(max_length = 800, default = '', null = False, verbose_name=("Description"))
+    User_ID = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=("User ID"))
     Status = models.CharField(max_length = 6, choices = ArticleStatus.choices, default = ArticleStatus.SAVED)
+    Created = models.DateTimeField(auto_now_add = True, verbose_name = "Creation Date")
     def _str_(self):
         return self.Article_Description
     objects = models.Manager()
 
 class Search(models.Model):
-    Search_ID = models.AutoField(primary_key = True)
+    class Meta():
+        verbose_name_plural = "Searches"
+
+    Search_ID = models.AutoField(primary_key = True, verbose_name=("Search ID"))
     Keywords = models.CharField(max_length = 100, default = '')
-    User_ID = models.ForeignKey(User, on_delete=models.CASCADE)
-    Article_ID = models.ForeignKey(Article, on_delete=models.CASCADE)
+    User_ID = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=("User ID"))
+    Article_ID = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name=("Article ID"))    
+    Created = models.DateTimeField(auto_now_add = True, verbose_name = "Creation Date")
     def _str_(self):
         return self.Search_ID
     objects = models.Manager()
