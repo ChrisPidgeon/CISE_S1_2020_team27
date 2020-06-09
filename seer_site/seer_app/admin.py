1d765c7f2bd11c54adf3ab3f0c3bf20c9aa941ed
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-06-09 12:37:52 +1200

    Added permission checking and role isolation in admin.py

--- seer_site/seer_app/admin.py
+++ seer_site/seer_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib import messages
 from django.utils.translation import ngettext
+from django.contrib.auth import get_permission_codename
 
 from .models import User
 from .models import Account
@@ -10,6 +11,8 @@ from .models import Article
 from .models import Search
 
 admin.site.site_header = "SEER Administration"
+admin.site.site_title = "SEER Administration Portal"
+
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('User_ID', 'F_name', 'L_name', 'Email')
@@ -32,26 +35,78 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('Created',)
    
     def make_submitted(self, request, queryset):
-        queryset.update(Status = "SUB")
+        rows_updated = queryset.update(Status = "SUB")
+        if rows_updated == 1:
+            message_bit = "1 article was"
+        else:
+            message_bit = "%s articles were" % rows_updated
+        self.message_user(request, "%s successfully marked as submitted." % message_bit)
+
     make_submitted.short_description = "Mark selected articles as submitted"
 
     def make_in_moderation(self, request, queryset):
         queryset.update(Status = "IN_MOD")
+        rows_updated = queryset.update(Status = "SUB")
+        if rows_updated == 1:
+            message_bit = "1 article was"
+        else:
+            message_bit = "%s articles were" % rows_updated
+        self.message_user(request, "%s successfully marked as in moderation." % message_bit)
+
     make_in_moderation.short_description = "Mark selected articles as in moderation"
 
     def make_moderated(self, request, queryset):
         queryset.update(Status = "MOD")
+        rows_updated = queryset.update(Status = "SUB")
+        if rows_updated == 1:
+            message_bit = "1 article was"
+        else:
+            message_bit = "%s articles were" % rows_updated
+        self.message_user(request, "%s successfully marked as moderated." % message_bit)
+
     make_moderated.short_description = "Mark selected articles as moderated"
 
     def make_in_analysis(self, request, queryset):
         queryset.update(Status = "IN_ANA")
+        rows_updated = queryset.update(Status = "SUB")
+        if rows_updated == 1:
+            message_bit = "1 article was"
+        else:
+            message_bit = "%s articles were" % rows_updated
+        self.message_user(request, "%s successfully marked as in analysis." % message_bit)
+
     make_in_analysis.short_description = "Mark selected articles as in analysis"
     
     def make_analyzed(self, request, queryset):
         queryset.update(Status = "ANA")
+        rows_updated = queryset.update(Status = "SUB")
+        if rows_updated == 1:
+            message_bit = "1 article was"
+        else:
+            message_bit = "%s articles were" % rows_updated
+        self.message_user(request, "%s successfully marked as analyzed." % message_bit)
+
     make_analyzed.short_description = "Mark selected articles as analyzed"
 
     actions = [make_submitted, make_in_moderation, make_moderated, make_in_analysis, make_analyzed]
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'MODERATOR' in request.user.username.upper():
+            if 'make_submitted' in actions:
+                del actions['make_submitted']
+            if 'make_in_analysis' in actions:
+                del actions['make_in_analysis']
+            if 'make_analyzed' in actions:
+                del actions['make_analyzed']
+        elif 'ANALYST' in request.user.username.upper():
+            if 'make_submitted' in actions:
+                del actions['make_submitted']
+            if 'make_in_moderation' in actions:
+                del actions['make_in_moderation']
+            if 'make_moderated' in actions:
+                del actions['make_moderated']
+        return actions
 
 class SearchAdmin(admin.ModelAdmin):
     list_display = ('Search_ID', 'Keywords')
