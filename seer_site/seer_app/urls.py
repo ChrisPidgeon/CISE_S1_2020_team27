9031f3b8fffb4f75284de3fc747a5d9456ddd3f9
Vaandite <53281594+Vaandite@users.noreply.github.com>
2020-06-11 21:19:04 +1200

    Tidy up all html pages

--- seer_site/seer_app/urls.py
+++ seer_site/seer_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', index, name='Home'),
     path('submit/', submit, name='Submit'),
     path('search/',search,name='search_results'),
-    path('advancedsearch/',advancedsearch,name='advancedsearch'),
+    path('advanced/',advancedsearch,name='AdvancedSearch'),
 
     url(r'^$',search,name='searchposts'),
     url(r'^$',advancedsearch,name='advancedsearch'),
